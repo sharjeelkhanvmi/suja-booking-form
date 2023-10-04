59c978d92613907ccdb3e8bd367e65599cffdc7a
Sharjeel Khan <sharjeelaslam96@gmail.com>
2023-10-04 20:41:52 +0500

    3 boxes section make it global to all steps
    3 boxes section make it global to all steps

---
+++ app/components/3boxes.jsx
@@ -0,0 +1,85 @@
+export default function Example() {
+
+  return (
+    <div className="p-4 justify-center gap-5 grid lg:grid-cols-3">
+    <div className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50">
+      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          aria-hidden="true"
+          className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zM12 2.25V4.5m5.834.166l-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243l-1.59-1.59"
+          ></path>
+        </svg>
+      </div>
+      <div className="flex-auto">
+        <a href="#" className="block font-semibold text-gray-900">
+          Learn from the best <span className="absolute inset-0"></span>
+        </a>
+      </div>
+    </div>
+    <div className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50">
+      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          aria-hidden="true"
+          className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M7.864 4.243A7.5 7.5 0 0119.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 004.5 10.5a7.464 7.464 0 01-1.15 3.993m1.989 3.559A11.209 11.209 0 008.25 10.5a3.75 3.75 0 117.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 01-3.6 9.75m6.633-4.596a18.666 18.666 0 01-2.485 5.33"
+          ></path>
+        </svg>
+      </div>
+      <div className="flex-auto">
+        <a href="#" className="block font-semibold text-gray-900">
+          Support at every step <span className="absolute inset-0"></span>
+        </a>
+      </div>
+    </div>
+    <div className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50">
+      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          aria-hidden="true"
+          className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z"
+          ></path>
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z"
+          ></path>
+        </svg>
+      </div>
+      <div className="flex-auto">
+        <a href="#" className="block font-semibold text-gray-900">
+          100% Money Back Guarantee{" "}
+          <span className="absolute inset-0"></span>
+        </a>
+      </div>
+    </div>
+  </div>
+  )
+}
