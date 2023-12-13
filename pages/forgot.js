7e40d41ac3616cb123e47b3e9ed3f46024aa9873
AhmedFrontEnd-JS-ReactJS-VueJS-NodeJS <ahmed.vibrant1@gmail.com>
2023-12-13 20:37:14 +0500

    capitalized
    capitalized

--- pages/forgot.js
+++ pages/forgot.js
@@ -41,7 +41,7 @@ export default function Home() {
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-red-800 dark:border-red-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-red-600 md:text-2xl dark:text-white">
-                FORGOT PASSWORD
+                Forgot Password
               </h1>
               <form onSubmit={handleSubmit} className=" space-y-4 md:space-y-6" action="#">
                 <div className='text-left'>
