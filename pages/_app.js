073bd7e64889865ed12447a355587f4202fe90a8
Sharjeel Khan <sharjeelaslam96@gmail.com>
2024-01-04 12:46:23 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 import { ContexProvider } from "@/app/context/MyContext"; // Adjust the path accordingly
 import { AuthProvider } from '@/app/context/AuthContext';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 //import drivingCoursesData from '@/database/models/drivingCoursesData';
 
 export default function App({ Component, pageProps }) {
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
       <Component {...pageProps} />
-      <SpeedInsights/>
+      
     </AuthProvider>
   );
 } 
