0042b102ef1a7f3d92a7041cca45b8b10a5a32bc
AhmedFrontEnd-JS-ReactJS-VueJS-NodeJS <ahmed.vibrant1@gmail.com>
2024-01-09 16:27:06 +0500

    test
    test

--- pages/api/leads/userFilter/index.js
+++ pages/api/leads/userFilter/index.js
@@ -8,23 +8,16 @@ export default async function POST(req, res) {
     await connectionSuja();
     // Check if orderId is provided
     if (!orderId ) {
-       
       return res.status(200).json({ error: "orderId is required" });
-    
     }
     const result = await Lead.findById({ _id: orderId });
     // Check if order with the given orderId exists
     if (!result) {
        return res.status(400).json({ error: "Order not found" });
-  
     }
-    
-    // Send the data in the response
     res.status(200).json({ filterData: result });
   } catch (error) {
     console.error("Error in userFilter Index Api", error);
-
-    // Send a generic error response for internal server errors
     res.status(500).json({ error: "Internal Server Error" });
   }
 }
