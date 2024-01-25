352ef02af635facd1527129d5d4b7c86cb3c1459
AhmedFrontEnd-JS-ReactJS-VueJS-NodeJS <ahmed.vibrant1@gmail.com>
2024-01-25 18:59:58 +0500

    Update user.js

--- app/service/user.js
+++ app/service/user.js
@@ -2,21 +2,12 @@ import dbconstr from "@/database/dbconstr"; // Assume you have a utility functio
 import Lead from '@/database/models/Lead';
 
 const users = async () => {
-  return [1,2,3,4,5]
   try {
     await dbconstr();
-
-    const leads = await Lead.find({ del: 0 })
-      .limit(5)
-      .skip(0)
-      .sort({ createdAt: -1 })
-      .populate("user")
-      .exec();
-
+    const leads = await Lead.find({ del: 0 }).limit(5).skip(0).sort({ createdAt: -1 }).populate("user").exec();
     return JSON.parse(JSON.stringify(leads));
   } catch (error) {
-    console.error(error);
-    return error; // Rethrow the error to handle it in the calling function
+    throw error
   }
 };
 
