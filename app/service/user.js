b0c10d6c39a13811ec1a8c50b771e7af4aece38b
AhmedFrontEnd-JS-ReactJS-VueJS-NodeJS <ahmed.vibrant1@gmail.com>
2024-01-25 19:24:30 +0500

    Update user.js

--- app/service/user.js
+++ app/service/user.js
@@ -3,7 +3,7 @@ import Lead from '@/database/models/Lead';
 await dbconstr();
 const users = async () => {
   try {
-    const leads = await Lead.find({ del: 0 });
+    const leads = await Lead.find({ del: 0 }).limit(5).skip(0).sort({ createdAt: -1 }).populate("user").exec();
     return JSON.parse(JSON.stringify(leads));
   } catch (error) {
     throw error
