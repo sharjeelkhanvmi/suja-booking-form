e4f0b63407518d933f40ab7c8218ef2c3e9e7ac5
AhmedFrontEnd-JS-ReactJS-VueJS-NodeJS <ahmed.vibrant1@gmail.com>
2024-01-25 19:00:58 +0500

    Update user.js

--- app/service/user.js
+++ app/service/user.js
@@ -1,9 +1,8 @@
 import dbconstr from "@/database/dbconstr"; // Assume you have a utility function for connecting to the database
 import Lead from '@/database/models/Lead';
-
+await dbconstr();
 const users = async () => {
   try {
-    await dbconstr();
     const leads = await Lead.find({ del: 0 }).limit(5).skip(0).sort({ createdAt: -1 }).populate("user").exec();
     return JSON.parse(JSON.stringify(leads));
   } catch (error) {
@@ -14,5 +13,4 @@ const users = async () => {
 const create = () => {
   console.log("user create");
 };
-
 export { users, create };
